fd2edcc954a4baf05cb4be88a038ba5c0a0dfa11
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2023-01-06 21:35:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+# API and Secret Key
 API_KEY = "<API KEY>"
 SECRET_KEY = "<SECRET KEY>"
+
+# Arbitrage minimum percentage difference required (in decimal)
+ARB_DIFF_REQ = 0.001
+
+# Wait time between each quote (since API calls are limited in free trial)
+wait_time = 0.5
